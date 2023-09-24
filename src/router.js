f195e362d221fc419fcce02f82efcc3847047e20
unknown <u201912404@upc.edu.pe>
2023-09-24 00:40:16 -0500

    feature(registerStudent)::add Register Student Added

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import WelcomeView from "@/views/WelcomeView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 
 
 const routes = [
     { path: "/", component: WelcomeView },
+    { path: "/register-student", component: RegisterView}
 ];
 
 const router = createRouter({
