611cf34ab35be3497282a1508674f7990d989c92
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-09-25 17:51:22 -0500

    feat(chooseSchedule): added schedule component

--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import WelcomeView from "@/views/WelcomeView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import LoginView from "@/views/LoginView.vue";
+import ChooseScheduleView from "@/views/ChooseScheduleView.vue";
 
 
 const routes = [
     { path: "/", component: WelcomeView },
     { path: "/register-student", component: RegisterView},
-    { path: "/login-student", component: LoginView}
+    { path: "/login-student", component: LoginView},
+    { path: "/choose-schedule/:id", component: ChooseScheduleView}
 ];
 
 const router = createRouter({
---
+++ src/views/ChooseScheduleView.vue
@@ -0,0 +1,17 @@
+<script>
+
+import {defineComponent} from "vue";
+import ChooseSchedule from "@/components/ChooseSchedule.vue";
+
+export default defineComponent({
+  components: {ChooseSchedule}
+})
+</script>
+
+<template>
+<ChooseSchedule v-bind:id="this.$route.params.id"></ChooseSchedule>
+</template>
+
+<style scoped>
+
+</style>
