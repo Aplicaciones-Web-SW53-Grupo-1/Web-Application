4b0edb05af2d9115a89bbad8fccbf01bbef84b84
Kurt Puican Salas <u202016643@upc.edu.pe>
2023-09-25 21:10:29 -0500

    feature(ratings):Add ratings component

--- src/router.js
+++ src/router.js
@@ -2,16 +2,16 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import WelcomeView from "@/views/WelcomeView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import LoginView from "@/views/LoginView.vue";
-import SelectetTutorView from "./views/SelectetTutorView.vue";
+import SelectedTutorView from "./views/SelectetTutorView.vue";
 import chatView from "@/views/chatView.vue";
 import ratingView from "@/views/RatingView.vue";
 
 const routes = [
     { path: "/", component: WelcomeView },
     { path: "/register-student", component: RegisterView},
-    { path: "/login-student", component: LoginView}
+    { path: "/login-student", component: LoginView},
     { path: "/rating", component:ratingView },
-    { path: "/selected", component:SelectetTutorView },
+    { path: "/selected", component:SelectedTutorView },
     { path: "/chat", component:chatView },
 ];
 
