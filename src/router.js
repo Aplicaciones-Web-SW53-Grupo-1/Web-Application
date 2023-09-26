7c66f0b8613a7344f5782064c64cfb093d80ea74
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-09-25 21:04:17 -0500

    feat(payment): added payment component and service for fake api

--- src/router.js
+++ src/router.js
@@ -2,12 +2,15 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import WelcomeView from "@/views/WelcomeView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import LoginView from "@/views/LoginView.vue";
+import PaymentView from "@/views/PaymentView.vue";
 
 
 const routes = [
     { path: "/", component: WelcomeView },
     { path: "/register-student", component: RegisterView},
-    { path: "/login-student", component: LoginView}
+    { path: "/login-student", component: LoginView},
+
+    { path: "/payment", component: PaymentView}
 ];
 
 const router = createRouter({
