902f6ae73908ae39651cc218b69622ae8d0e233e
Kurt Puican Salas <u202016643@upc.edu.pe>
2023-09-25 20:47:52 -0500

    feature(selected-tutor):Add selected tutor view

---
+++ src/views/SelectetTutorView.vue
@@ -0,0 +1,16 @@
+<script>
+import {defineComponent} from "vue";
+import SelectedTutor from "@/components/SelectedTutor.vue";
+
+export default defineComponent({
+  components:{SelectedTutor}
+})
+</script>
+
+<template>
+  <SelectedTutor/>
+</template>
+
+<style scoped>
+
+</style>
