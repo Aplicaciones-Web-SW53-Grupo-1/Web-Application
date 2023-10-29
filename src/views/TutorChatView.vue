6444557336e51cebf553e8c0446be64d084a0a48
Rafael Luyo <u201919295@upc.edu.pe>
2023-10-29 11:34:04 -0500

    feat(tutorchat): add tutorchat component and view

---
+++ src/views/TutorChatView.vue
@@ -0,0 +1,17 @@
+<script>
+import {defineComponent} from "vue";
+
+import TutorChat from "@/components/TutorChat.vue";
+
+export default defineComponent({
+  components:{TutorChat}
+})
+</script>
+
+<template>
+  <TutorChat/>
+</template>
+
+<style scoped>
+
+</style>
